56a9b2c6f232ec8c49312e85af0174d1c73539f5
William Hua <william@attente.ca>
2018-02-15 16:11:32 -0700

    provider -> truffle

--- client/ttt/index.ts
+++ client/ttt/index.ts
@@ -1,8 +1,8 @@
 import * as ethers from 'ethers'
 
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:9545')
+const truffle = new ethers.providers.JsonRpcProvider('http://localhost:9545')
 const contract = require('../../build/contracts/TTT.json')
-const ttt = new ethers.Contract('0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f', contract.abi, provider)
+const ttt = new ethers.Contract('0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f', contract.abi, truffle)
 
 console.log(ttt)
 

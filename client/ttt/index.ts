edb8b66fd7eabcaf4bcfbfaa311bf7944e4aa4a9
William Hua <william@attente.ca>
2018-02-15 17:04:25 -0700

    test DGame.isSubkeySigned

--- client/ttt/index.ts
+++ client/ttt/index.ts
@@ -37,3 +37,52 @@ ttt.nextState(mState, mMove).then((mState) => {
   console.log(mState[0])
 
 })
+
+const game = '0x0123456789012345678901234567890123456789'
+const matchID = 12345
+const matchIDHex = ethers.utils.hexlify(ethers.utils.padZeros(ethers.utils.arrayify(ethers.utils.hexlify(matchID)), 4))
+const subkey = ethers.Wallet.createRandom()
+const message = `Sign to play! This won't cost anything.\n\nGame: ${game}\nMatch: ${matchIDHex}\nPlayer: ${subkey.getAddress().toLowerCase()}`
+const metamask = new ethers.providers.Web3Provider((global as any).web3.currentProvider)
+
+metamask.listAccounts().then((accounts) => {
+  metamask.getSigner(accounts[0]).signMessage(message).then((signature) => {
+    const signatureBytes = ethers.utils.arrayify(signature)
+
+    const dMatch = {
+      game: game,
+      matchID: matchID,
+      players: [
+        {
+          account: accounts[0],
+          subkey: subkey.getAddress(),
+          signature: {
+            v: signatureBytes[64],
+            r: ethers.utils.hexlify(new Uint8Array(signatureBytes.buffer, 0, 32)),
+            s: ethers.utils.hexlify(new Uint8Array(signatureBytes.buffer, 32, 32))
+          }
+        },
+        {
+          account: accounts[0],
+          subkey: subkey.getAddress(),
+          signature: {
+            v: signatureBytes[64],
+            r: ethers.utils.hexlify(new Uint8Array(signatureBytes.buffer, 0, 32)),
+            s: ethers.utils.hexlify(new Uint8Array(signatureBytes.buffer, 32, 32))
+          }
+        }
+      ],
+      signature: {
+        v: 0,
+        r: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        s: '0x0000000000000000000000000000000000000000000000000000000000000000'
+      }
+    }
+
+    dMatch['[object Object]'] = dMatch.players // XXX
+
+    ttt.isSubkeySigned(dMatch, 0).then((signed) => {
+      console.log(`ttt.isSubkeySigned: ${signed}`)
+    })
+  })
+})

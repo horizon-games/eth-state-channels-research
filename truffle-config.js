75353f1b8b31eafddace039dfc69c5deba536934
William Hua <william@attente.ca>
2018-03-07 16:19:50 -0500

    set truffle search directories

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  contracts_directory: "ethereum",
+  migrations_directory: "ethereum",
   networks: {
     ganache: {
       host: "127.0.0.1",

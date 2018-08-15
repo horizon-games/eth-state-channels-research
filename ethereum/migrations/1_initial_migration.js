b359411dac922fee936cc5f5dc440ea2f01850bd
José Carlos Nieto <jose.carlos@menteslibres.net>
2018-08-15 17:35:25 +0000

    ethereum: use a local ethereum network

--- ethereum/migrations/1_initial_migration.js
+++ ethereum/migrations/1_initial_migration.js
@@ -1,9 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations, {
-    gasPrice: 1,
-    gasLimit: 4000000000000000,
-    gas: 4000000000000000
-  });
+  deployer.deploy(Migrations);
 };
--- ethereum/migrations/2_deploy_arcadeum.js
+++ ethereum/migrations/2_deploy_arcadeum.js
@@ -1,9 +1,5 @@
 var Arcadeum = artifacts.require("./Arcadeum.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Arcadeum, {
-    gasPrice: 1,
-    gasLimit: 4000000000000000,
-    gas: 4000000000000000
-  });
+  deployer.deploy(Arcadeum);
 };

d529030acfd15e8bda732d806beaa1d119b619e7
José Carlos Nieto <jose.carlos@menteslibres.net>
2018-08-15 17:35:25 +0000

    ethereum: use a local ethereum network

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -4,13 +4,13 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    ganache: {
+    local: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
       gasPrice: 1,
       gasLimit: 4000000000000000,
-      gas: 4000000000000000
+      gas: 9000000000
     },
     rinkeby: {
       provider: function () {
